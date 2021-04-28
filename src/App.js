1d468fa5ee5844d61dd4c6bd49c6d835cda21a7c
Nant1ng <andressantana99@hotmail.se>
2021-04-28 20:33:09 +0200

    Commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from "./logo.svg";
+import "./App.css";
+import AppRoute from "./components/AppRoute";
+import API from "./components/API";
+
+
+// 13.00  // den här error som vi fick i useEffect
+// assets hur kan vi rendera background image i react
+// en valig image i react
+
+function App() {
+  return (
+    <div className="App">
+      <AppRoute />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/AppRoute.js
@@ -0,0 +1,32 @@
+import React from "react";
+import CardList from "./card/CardList";
+import Form from "./Form";
+import Menu from "./Menu"
+
+import {Route, BrowserRouter as Router} from "react-router-dom"
+// react-router-dom ?? 
+
+// yarn add react-router-dom
+
+export default function AppRoute(){
+
+    return(
+
+        <>
+
+        <Router>  
+              
+          <Menu />
+
+           <Route path="/" exact component={CardList} />
+           <Route path="/form" component={Form} />
+
+        </Router>
+           
+           
+           
+        </>
+    )
+}
+
+// kl. 10.00
