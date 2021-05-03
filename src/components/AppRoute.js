d2d868cb9f4e31f726912a32b33b8654d41f0a6d
Nant1ng <andressantana99@hotmail.se>
2021-05-03 16:13:00 +0200

    Commit

--- src/components/AppRoute.js
+++ src/components/AppRoute.js
@@ -1,32 +1,28 @@
 import React from "react";
 import CardList from "./card/CardList";
 import Form from "./Form";
-import Menu from "./Menu"
+import Menu from "./Menu";
+import Registration from "./Registration";
+import Login from "./Login"
 
-import {Route, BrowserRouter as Router} from "react-router-dom"
-// react-router-dom ?? 
+import { Route, BrowserRouter as Router } from "react-router-dom";
+// react-router-dom ??
 
 // yarn add react-router-dom
 
-export default function AppRoute(){
+export default function AppRoute() {
+  return (
+    <>
+      <Router>
+        <Menu />
 
-    return(
-
-        <>
-
-        <Router>  
-              
-          <Menu />
-
-           <Route path="/" exact component={CardList} />
-           <Route path="/form" component={Form} />
-
-        </Router>
-           
-           
-           
-        </>
-    )
+        <Route path="/" exact component={CardList} />
+        <Route path="/form" component={Form} />
+        <Route path="/register" component={Registration} />
+        <Route path="/login" component= {Login} />
+      </Router>
+    </>
+  );
 }
 
 // kl. 10.00
